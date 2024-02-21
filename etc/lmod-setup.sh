7ac24dadbde5dbf417687d12acdaad2a956110f2
jderrico-noaa <janet.m.derrico@noaa.gov>
2024-02-21 23:14:31 +0000

    add lmod-setup.sh and etc dir

---
+++ etc/lmod-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# = 0 ]; then
+   L_MACHINE=${MACHINE}
+   cat << EOF_USAGE
+Usage: source etc/lmod-setup.sh PLATFORM
+
+OPTIONS:
+   PLATFORM - name of machine you are building on
+      (e.g. hera | jet)
+EOF_USAGE
+   exit 1
+else
+   L_MACHINE=$1
+fi
+
+if [ "$L_MACHINE" != wcoss2 ]; then
+  [[ ${SHELLOPTS} =~ nounset ]] && has_mu=true || has_mu=false
+  [[ ${SHELLOPTS} =~ errexit ]] && has_me=true || has_me=false
+  $has_mu && set +u
+  $has_me && set +e
+  source /etc/profile
+  $has_mu && set -u
+  $has_me && set -e
+fi
+
+if [ "$L_MACHINE" = hera ] || [ "$L_MACHINE" = jet ]; then
+   module purge
+else
+   echo "ERROR: Unsupported platform ${L_MACHINE}"
+   return 1
+fi
