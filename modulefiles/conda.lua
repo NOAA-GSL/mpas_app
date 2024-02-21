bb9d43bd17e64f603b2aadffad783cc24a5d5294
jderrico-noaa <janet.m.derrico@noaa.gov>
2024-02-21 22:47:06 +0000

    add conda.lua

---
+++ modulefiles/conda.lua
@@ -0,0 +1,16 @@
+help([[
+This module loads python environement for running the MPAS Workflow on
+the NOAA RDHPC machine Jet
+]])
+
+whatis([===[Loads libraries needed for running the MPAS Workflow on Jet ]===])
+
+load("rocoto")
+
+load("conda")
+
+if mode() == "load" then
+   LmodMsgRaw([===[Please do the following to activate conda:
+       > conda activate mpas_workflow
+]===])
+end
