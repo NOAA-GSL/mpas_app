ff8df5f49c97ed8777ca0ce6caedc28df23d58ed
Christina Holt <56881914+christinaholtNOAA@users.noreply.github.com>
2024-04-02 09:07:38 -0600

    Add init_atmosphere and MPAS to the workflow. (#7)
    * Adding new workflow files.
    
    * Cleaning up.
    
    * Format and lint
    
    * Update to official environment.
    
    * That was unnecessary duplication...
    
    * Adding MPAS components
    
    * Use more cores for the forecast.
    
    * Changing FV3GFS -> GFS
    
    * Missed FV3GFS -> GFS in the runscript

---
+++ scripts/mpas.py
@@ -0,0 +1,33 @@
+"""
+The run script for the mpas forecast
+"""
+
+import os
+from datetime import datetime
+from pathlib import Path
+
+from uwtools.api import config as uwconfig
+from uwtools.api import mpas
+
+
+# Load the YAML config
+CONFIG_PATH = os.environ["CONFIG_PATH"]
+CYCLE = os.environ["CYCLE"]
+
+cycle = datetime.fromisoformat(CYCLE)
+
+# Extract driver config from experiment config and save in the run directory
+expt_config = uwconfig.get_yaml_config(CONFIG_PATH)
+expt_config.dereference(context={"cycle": cycle, **expt_config})
+
+
+mpas_config = expt_config["forecast"]
+mpas_config.update({"platform": expt_config["platform"], "user": expt_config["user"]})
+mpas_config = uwconfig.get_yaml_config(mpas_config)
+
+mpas_yaml = Path(mpas_config["mpas"]["run_dir"]) / "mpas.yaml"
+mpas_yaml.parent.mkdir(parents=True, exist_ok=True)
+mpas_config.dump(path=mpas_yaml)
+
+# Run mpas
+mpas.execute(task="run", config=mpas_yaml, cycle=cycle)
---
+++ scripts/mpas_init.py
@@ -0,0 +1,34 @@
+"""
+The run script for the mpas init_atmosphere
+"""
+
+import os
+from datetime import datetime
+from pathlib import Path
+
+from uwtools.api import config as uwconfig
+from uwtools.api import mpas_init
+
+
+# Load the YAML config
+CONFIG_PATH = os.environ["CONFIG_PATH"]
+CYCLE = os.environ["CYCLE"]
+EXPT_SECT = os.environ["EXPT_SECT"]
+
+cycle = datetime.fromisoformat(CYCLE)
+
+# Extract driver config from experiment config and save in the run directory
+expt_config = uwconfig.get_yaml_config(CONFIG_PATH)
+expt_config.dereference(context={"cycle": cycle, **expt_config})
+
+
+mpas_init_config = expt_config[EXPT_SECT]
+mpas_init_config.update({"platform": expt_config["platform"], "user": expt_config["user"]})
+mpas_init_config = uwconfig.get_yaml_config(mpas_init_config)
+
+mpas_init_yaml = Path(mpas_init_config["mpas_init"]["run_dir"]) / "mpas_init.yaml"
+mpas_init_yaml.parent.mkdir(parents=True, exist_ok=True)
+mpas_init_config.dump(path=mpas_init_yaml)
+
+# Run mpas_init
+mpas_init.execute(task="run", config=mpas_init_yaml, cycle=cycle)
