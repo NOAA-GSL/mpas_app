0a8e8cc7992d24891263bee4b7ef57c13fdadcc9
jderrico-noaa <janet.m.derrico@noaa.gov>
2024-04-18 20:43:32 +0000

    change intel version for hera build

--- modulefiles/build_hera_intel.lua
+++ modulefiles/build_hera_intel.lua
@@ -1,24 +1,21 @@
 help([[
 This module loads libraries for building the MPAS App on
-the NOAA RDHPC machine Hera using Intel-2023.2.0
+the NOAA RDHPC machine Hera using Intel-2022.1.2
 ]])
 
 whatis([===[Loads libraries needed for building the MPAS App on Hera ]===])
 
 load("cmake/3.28.1")
 load("gnu")
-load("intel/2023.2.0")
-load("impi/2023.2.0")
+load("intel/2022.1.2")
+load("impi/2022.1.2")
 
---prepend_path("PATH", "/apps/oneapi/mpi/2021.10.0/bin")
---prepend_path("LD_LIBRARY_PATH", "/apps/oneapi/mpi/2021.10.0/lib")
-
-load("pnetcdf/1.12.3")
+load("pnetcdf/1.11.2")
 load("szip")
-load("hdf5parallel/1.10.5")
+load("hdf5parallel/1.10.6")
 load("netcdf-hdf5parallel/4.7.0")
 
-setenv("PNETCDF", "/apps/pnetcdf/1.12.3/intel_2023.2.0-impi")
+setenv("PNETCDF", "/apps/pnetcdf/1.11.2/intel/2022.1.2")
 setenv("CMAKE_C_COMPILER", "mpiicc")
 setenv("CMAKE_CXX_COMPILER", "mpiicpc")
 setenv("CMAKE_Fortran_COMPILER", "mpiifort")
