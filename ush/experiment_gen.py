cf4de7dfe3842905dc10115e8b02c2d89dace51c
Christina.Holt <Christina.Holt@noaa.gov>
2024-04-25 22:36:58 +0000

    Pull out platform specifics

--- ush/experiment_gen.py
+++ ush/experiment_gen.py
@@ -13,40 +13,45 @@ from typing import Optional
 import uwtools.api.config as uwconfig
 import uwtools.api.rocoto as uwrocoto
 
+
 def create_grid_files(expt_dir: Path, mesh_file_path: Path, nprocs: int) -> None:
     """
     Stage the mesh file in the experiment directory and decompose them for the current experiment.
     """
     copy(src=mesh_file_path, dst=expt_dir)
     mesh_file = expt_dir / mesh_file_path.name
-    cmd = f'gpmetis -minconn -contig -niter=200 {mesh_file} {nprocs}'
+    cmd = f"gpmetis -minconn -contig -niter=200 {mesh_file} {nprocs}"
     try:
-        output = check_output(cmd, encoding="utf=8", shell=True,
-                stderr=STDOUT, text=True)
+        output = check_output(
+            cmd, encoding="utf=8", shell=True, stderr=STDOUT, text=True
+        )
     except CalledProcessError as e:
         output = e.output
         print("Error running command:")
         print(f"  {cmd}")
         for line in output.split("\n"):
-           print(line)
+            print(line)
         print(f"Failed with status: {e.returncode}")
         sys.exit(1)
 
-def main(user_config_file: Optional[Path]) -> None:
 
+def main(user_config_file: Optional[Path]) -> None:
     """
     Stage the Rocoto XML and experiment YAML in the desired experiment
     directory.
     """
 
     # Set up the experiment
+    mpas_app = Path(os.path.dirname(__file__)).parent.absolute()
     experiment_config = uwconfig.get_yaml_config(Path("./default_config.yaml"))
     user_config = uwconfig.get_yaml_config(user_config_file)
-    experiment_config.update_values(user_config)
+    machine = user_config["user"]["platform"]
+    platform_config = uwconfig.get_yaml_config(mpas_app / "parm" / "machines" / f"{machine}.yaml")
 
-    experiment_config["user"]["mpas_app"] = (
-        Path(os.path.dirname(__file__)).parent.absolute().as_posix()
-    )
+    for config in (platform_config, user_config):
+        experiment_config.update_values(config)
+
+    experiment_config["user"]["mpas_app"] = mpas_app.as_posix()
     experiment_config.dereference()
 
     # Build the experiment directory
@@ -70,14 +75,19 @@ def main(user_config_file: Optional[Path]) -> None:
         sys.exit(1)
 
     # Create grid files
-    mesh_file_name = f"{experiment_config['user']['grid_label']}.graph.info"
-    mesh_file_path = Path(experiment_config["user"]["mesh_files"]) / mesh_file_name
+    mesh_file_name = f"{experiment_config['user']['mesh_label']}.graph.info"
+    mesh_file_path = Path(experiment_config["data"]["mesh_files"]) / mesh_file_name
     all_nprocs = (
-        experiment_config[tasks.split(".")[0]][tasks.split(".")[1]]["execution"]["batchargs"]["cores"]
-        for tasks in ("create_ics.mpas_init", "create_lbcs.mpas_init", "forecast.mpas")
+        experiment_config[sect][driver]["execution"]["batchargs"]["cores"]
+        for sect, driver in (
+            ("create_ics", "mpas_init"),
+            ("create_lbcs", "mpas_init"),
+            ("forecast", "mpas"),
         )
+    )
     for nprocs in all_nprocs:
         create_grid_files(experiment_path, mesh_file_path, nprocs)
+
 
 if __name__ == "__main__":
 
