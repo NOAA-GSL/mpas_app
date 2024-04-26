13720de0313e476b83ab4ccf3bcfcf5996b61d5f
Janet Derrico <143837053+jderrico-noaa@users.noreply.github.com>
2024-04-26 17:44:46 -0600

    Update modulefiles/build_hera_intel.lua
    Co-authored-by: Christina Holt <56881914+christinaholtNOAA@users.noreply.github.com>

--- modulefiles/build_hera_intel.lua
+++ modulefiles/build_hera_intel.lua
@@ -12,7 +12,7 @@ load("gnu")
 load("intel/2022.1.2")
 load("impi/2022.1.2")
 
-load("pnetcdf/1.11.2")
+load("pnetcdf/1.7.0")
 load("szip")
 load("hdf5parallel/1.10.6")
 load("netcdf-hdf5parallel/4.7.0")
