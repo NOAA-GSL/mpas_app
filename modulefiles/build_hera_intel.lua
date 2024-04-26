4c1246b4d07876ea418d77180487c0b2a9b99ed8
Janet Derrico <143837053+jderrico-noaa@users.noreply.github.com>
2024-04-26 17:44:55 -0600

    Update modulefiles/build_hera_intel.lua
    Co-authored-by: Christina Holt <56881914+christinaholtNOAA@users.noreply.github.com>

--- modulefiles/build_hera_intel.lua
+++ modulefiles/build_hera_intel.lua
@@ -15,7 +15,7 @@ load("impi/2022.1.2")
 load("pnetcdf/1.7.0")
 load("szip")
 load("hdf5parallel/1.10.6")
-load("netcdf-hdf5parallel/4.7.0")
+load("netcdf-hdf5parallel/4.7.4")
 
 setenv("PNETCDF", "/apps/pnetcdf/1.11.2/intel/2022.1.2")
 setenv("CMAKE_C_COMPILER", "mpiicc")
