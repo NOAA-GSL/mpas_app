c04a6ce4f5be151694bcb6b8583aa725b7ef933a
Janet Derrico <143837053+jderrico-noaa@users.noreply.github.com>
2024-04-26 15:03:22 -0600

    Update modulefiles/build_hera_intel.lua
    Co-authored-by: Christina Holt <56881914+christinaholtNOAA@users.noreply.github.com>

--- modulefiles/build_hera_intel.lua
+++ modulefiles/build_hera_intel.lua
@@ -4,7 +4,9 @@ the NOAA RDHPC machine Hera using Intel-2022.1.2
 ]])
 
 whatis([===[Loads libraries needed for building the MPAS App on Hera ]===])
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/unified-env-rocky8/install/modulefiles/Core")
 
+load("stack-intel/2021.5.0")
 load("cmake/3.23.1")
 load("gnu")
 load("intel/2022.1.2")
