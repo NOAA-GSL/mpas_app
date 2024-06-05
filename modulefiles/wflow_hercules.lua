adba7f7d2c663ef9353284617809e2380bf04f67
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2024-06-05 13:48:04 -0500

    Add support for Hercules

---
+++ modulefiles/wflow_hercules.lua
@@ -0,0 +1,17 @@
+help([[
+This module loads the python environment for running the MPAS App on 
+the NOAA RDHPC machine Hera
+]]) 
+ 
+whatis([===[Loads libraries needed for running the MPAS App on Hera ]===]) 
+
+load("contrib")
+load("rocoto")
+load("conda")
+
+if mode() == "load" then
+    LmodMsgRaw([===[Please do the following to activate conda:
+	>conda activate mpas_app
+]===])
+end
+
