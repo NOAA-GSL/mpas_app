57c614b9cb88215a4f99b2362c78c27b8ab69540
Christina Holt <Christina.Holt@noaa.gov>
2024-06-05 16:22:27 +0000

    WIP

--- load_wflow_modules.sh
+++ load_wflow_modules.sh
@@ -2,6 +2,7 @@
 scrfunc_fp=$( readlink -f "${BASH_SOURCE[0]}" )
 scrfunc_dir=$( dirname "${scrfunc_fp}" )
 
+source $scrfunc_dir/etc/lmod-setup.sh
 module use $scrfunc_dir/modulefiles
 module load wflow_$1 > /dev/null 2>&1
 
