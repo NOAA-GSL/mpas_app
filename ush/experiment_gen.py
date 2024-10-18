efb7c4c802702213bea779053ac2ce6ff3917d72
Christina Holt <Christina.Holt@noaa.gov>
2024-10-18 22:24:25 +0000

    Pick up changes from the HFIP_2024 branch.

--- ush/experiment_gen.py
+++ ush/experiment_gen.py
@@ -3,6 +3,7 @@ Creates the experiment directory and populates it with necessary configuration a
 """
 
 import argparse
+import logging
 import os
 import sys
 from pathlib import Path
@@ -12,6 +13,7 @@ from typing import Optional
 
 import uwtools.api.config as uwconfig
 import uwtools.api.rocoto as uwrocoto
+from uwtools.config.formats.base import Config
 
 
 def create_grid_files(expt_dir: Path, mesh_file_path: Path, nprocs: int) -> None:
@@ -35,7 +37,7 @@ def create_grid_files(expt_dir: Path, mesh_file_path: Path, nprocs: int) -> None
         sys.exit(1)
 
 
-def main(user_config_file: Optional[Path]) -> None:
+def main(user_config_files: list[Path, str]) -> None:
     """
     Stage the Rocoto XML and experiment YAML in the desired experiment
     directory.
@@ -44,28 +46,45 @@ def main(user_config_file: Optional[Path]) -> None:
     # Set up the experiment
     mpas_app = Path(os.path.dirname(__file__)).parent.absolute()
     experiment_config = uwconfig.get_yaml_config(Path("./default_config.yaml"))
-    user_config = uwconfig.get_yaml_config(user_config_file)
+    user_config = None
+    for cfg_file in user_config_files:
+        cfg = uwconfig.get_yaml_config(cfg_file)
+        if not user_config:
+            user_config = cfg
+            continue
+        user_config.update_values(cfg)
+
     machine = user_config["user"]["platform"]
     platform_config = uwconfig.get_yaml_config(mpas_app / "parm" / "machines" / f"{machine}.yaml")
 
-    for config in (platform_config, user_config):
-        experiment_config.update_values(config)
+    for supp_config in (platform_config, user_config):
+        experiment_config.update_values(supp_config)
 
     experiment_config["user"]["mpas_app"] = mpas_app.as_posix()
-    experiment_config.dereference()
 
     # Build the experiment directory
     experiment_path = Path(experiment_config["user"]["experiment_dir"])
+    print("Experiment will be set up here: {}".format(experiment_path))
     os.makedirs(experiment_path, exist_ok=True)
 
     experiment_file = experiment_path / "experiment.yaml"
 
     # Load the workflow definition
-    default_workflow = Path("../parm/wflow/cold_start.yaml")
+    workflow_blocks = experiment_config["user"]["workflow_blocks"]
+    workflow_blocks = [mpas_app / "parm" / "wflow" / b for b in workflow_blocks]
+
+    workflow_config = None
+    for workflow_block in workflow_blocks:
+        if workflow_config is None:
+            workflow_config = uwconfig.get_yaml_config(workflow_block)
+        else:
+            workflow_config.update_values(uwconfig.get_yaml_config(workflow_block))
+    workflow_config.update_values(experiment_config)
+
     uwconfig.realize(
-        input_config=default_workflow,
+        input_config=workflow_config,
         output_file=experiment_file,
-        update_config=experiment_config,
+        update_config={},
     )
 
     # Create the workflow files
@@ -77,14 +96,19 @@ def main(user_config_file: Optional[Path]) -> None:
     # Create grid files
     mesh_file_name = f"{experiment_config['user']['mesh_label']}.graph.info"
     mesh_file_path = Path(experiment_config["data"]["mesh_files"]) / mesh_file_name
-    all_nprocs = (
-        experiment_config[sect][driver]["execution"]["batchargs"]["cores"]
-        for sect, driver in (
-            ("create_ics", "mpas_init"),
-            ("create_lbcs", "mpas_init"),
-            ("forecast", "mpas"),
-        )
-    )
+
+    experiment_config = uwconfig.get_yaml_config(config=experiment_file)
+    all_nprocs = []
+    for sect, driver in (
+        ("create_ics", "mpas_init"),
+        ("create_lbcs", "mpas_init"),
+        ("forecast", "mpas"),
+        ):
+        if sect in experiment_config:
+            resources = experiment_config[sect][driver]["execution"]["batchargs"]
+            if (cores := resources.get("cores")) is None:
+                cores = resources["nodes"] * resources["tasks_per_node"]
+            all_nprocs.append(cores)
     for nprocs in all_nprocs:
         if not (experiment_path / f"{mesh_file_path.name}.part.{nprocs}").is_file():
             print(f"Creating grid file for {nprocs} procs")
@@ -93,9 +117,16 @@ def main(user_config_file: Optional[Path]) -> None:
 
 if __name__ == "__main__":
 
+    logging.basicConfig(level=logging.INFO)
+
     parser = argparse.ArgumentParser(
         description="Configure an experiment with the following input:"
     )
-    parser.add_argument("user_config_file", help="Path to the user config file.")
+    parser.add_argument(
+            "user_config_files",
+            nargs="+",
+            help="Paths to the user config files.")
+
     args = parser.parse_args()
-    main(user_config_file=Path(args.user_config_file))
+    path_list = [Path(p) for p in args.user_config_files]
+    main(user_config_files=path_list)
