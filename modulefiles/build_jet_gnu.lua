9a44e34d163bab038fef47a7b7534b95d2ec44c8
Naureen <naureen.bharwani@noaa.gov>
2024-11-26 19:06:17 +0000

    Pair down build for gnu

--- modulefiles/build_jet_gnu.lua
+++ modulefiles/build_jet_gnu.lua
@@ -8,13 +8,6 @@ whatis([===[Loads libraries needed for building the MPAS Workflow on Jet ]===])
 
 load("cmake/3.28.1")
 load("gnu/14.2.0")
-load("intel/2023.2.0")
-load("impi/2023.2.0")
-
-load("pnetcdf/1.12.3")
-load("szip")
-load("hdf5parallel/1.10.5")
-load("netcdf-hdf5parallel/4.7.0")
 
 setenv("CMAKE_C_COMPILER","mpiicc")
 setenv("CMAKE_CXX_COMPILER", "mpiicc")
