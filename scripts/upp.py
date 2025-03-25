1c3dc89817eccd0da53e86de5481c97b788f8907
elcarpenterNOAA <emily.l.carpenter@noaa.gov>
2025-03-25 00:07:00 +0000

    clean up

--- scripts/upp.py
+++ scripts/upp.py
@@ -22,19 +22,9 @@ cycle = datetime.fromisoformat(CYCLE)
 upp_driver = UPP(config=CONFIG_PATH, cycle=cycle, leadtime=LEAD, key_path=["post"])
 upp_driver.run()
 
-# Extract driver config from experiment config
-# expt_config = uwconfig.get_yaml_config(CONFIG_PATH)
-# expt_config.dereference(context={"cycle": cycle, "leadtime": LEAD, **expt_config})
-
 # Obtain run directory path
 upp_dir = Path(upp_driver.config["rundir"])
 
-# driver_config = expt_config["post"]["upp"]
-# rundir = Path(driver_config["rundir"])
-# print(f"Will run in {rundir}")
-
-# Run upp
-# upp.execute(task="run", config=CONFIG_PATH, cycle=cycle, key_path=["post"], leadtime=LEAD)
 if not (upp_dir / "runscript.upp.done").is_file():
     print("Error occurred running UPP. Please see component error logs.")
     sys.exit(1)
