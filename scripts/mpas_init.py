682c9340f36a96e6404e2daa33ccc917254118fc
Christina Holt <56881914+christinaholtNOAA@users.noreply.github.com>
2025-04-28 16:08:07 -0600

    Refactor mpas_init and ungrib scripts. (#64)
    Refactor scripts to use argparse instead of environment variables, and allow for importing the script. The rocoto calls to the scripts are also updated.

--- scripts/mpas_init.py
+++ scripts/mpas_init.py
@@ -1,32 +1,59 @@
 #!/usr/bin/env python3
-
 """
 The run script for the MPAS init_atmosphere.
 """
 
-import os
+import datetime as dt
+import logging
 import sys
-from datetime import datetime
+from argparse import ArgumentParser
 from pathlib import Path
 
 from uwtools.api.logging import use_uwtools_logger
 from uwtools.api.mpas_init import MPASInit
 
 
-def main():
-    use_uwtools_logger()
+def parse_args(argv):
+    """
+    Parse arguments for the script.
+    """
+    parser = ArgumentParser(
+        description="Script that runs mpas_init via uwtools API.",
+    )
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        metavar="PATH",
+        required=True,
+        help="Path to experiment config file.",
+        type=Path,
+    )
+    parser.add_argument(
+        "--cycle",
+        help="The cycle in ISO8601 format (e.g. 2024-07-15T18).",
+        required=True,
+        type=dt.datetime.fromisoformat,
+    )
+    parser.add_argument(
+        "--key-path",
+        help="Dot-separated path of keys leading through the config to the driver's YAML block.",
+        metavar="KEY[.KEY...]",
+        required=True,
+        type=lambda s: s.split("."),
+    )
+    return parser.parse_args(argv)
 
-    # Load the YAML config.
-    config_path = os.environ["CONFIG_PATH"]
-    expt_sect = os.environ["EXPT_SECT"]
-    cycle = datetime.fromisoformat(os.environ["CYCLE"])
 
-    # Run MPAS Init.
-    mpas_init_driver = MPASInit(config=config_path, cycle=cycle, key_path=[expt_sect])
-    mpas_init_driver.run()
+def run_mpas_init(config_file, cycle, key_path):
+    """
+    Setup and run the mpas_init UW Driver.
+    """
 
-    # Obtain MPAS Init run directory path.
+    # Run mpas_init
+    mpas_init_driver = MPASInit(config=config_file, cycle=cycle, key_path=key_path)
     mpas_init_dir = Path(mpas_init_driver.config["rundir"])
+    logging.info("Will run mpas_init in {mpas_init_dir}")
+    mpas_init_driver.run()
 
     if not (mpas_init_dir / "runscript.mpas_init.done").is_file():
         print("Error occurred running mpas_init. Please see component error logs.")
@@ -34,4 +61,13 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+
+    use_uwtools_logger()
+
+
+    args = parse_args(sys.argv[1:])
+    run_mpas_init(
+        config_file=args.config_file,
+        cycle=args.cycle,
+        key_path=args.key_path,
+    )
---
+++ scripts/utils.py
@@ -0,0 +1,73 @@
+
+from __future__ import annotations
+
+import sys
+from subprocess import STDOUT, CalledProcessError, check_output
+from typing import TYPE_CHECKING
+
+from uwtools.logging import INDENT, log
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+def run_shell_cmd(
+    cmd: str,
+    cwd: Path | str | None = None,
+    env: dict[str, str] | None = None,
+    log_output: bool | None = False,
+    taskname: str | None = None,
+) -> tuple[bool, str]:
+    """
+    Run a command in a shell.
+
+    :param cmd: The command to run.
+    :param cwd: Change to this directory before running cmd.
+    :param env: Environment variables to set before running cmd.
+    :param log_output: Log output from successful cmd? (Error output is always logged.)
+    :param taskname: Name of task executing this command, for logging.
+    :return: A result object providing combined stderr/stdout output and success values.
+    """
+    pre = f"{taskname}: " if taskname else ""
+    msg = f"%sRunning: {cmd}"
+    if cwd:
+        msg += f" in {cwd}"
+    if env:
+        kvpairs = " ".join(f"{k}={v}" for k, v in env.items())
+        msg += f" with environment variables {kvpairs}"
+    log.info(msg, pre)
+    try:
+        output = check_output(
+            cmd, cwd=cwd, encoding="utf=8", env=env, shell=True, stderr=STDOUT, text=True
+        )
+        logfunc = log.info
+        success = True
+    except CalledProcessError as e:
+        output = e.output
+        log.error("%sFailed with status: %s", pre, e.returncode)
+        logfunc = log.error
+        success = False
+    if output and (log_output or not success):
+        logfunc("%sOutput:", pre)
+        for line in output.split("\n"):
+            logfunc("%s%s%s", pre, INDENT, line)
+    return success, output
+
+def walk_key_path(config, key_path):
+    """
+    Navigate to the sub-config at the end of the path of given keys.
+    """
+    keys = []
+    pathstr = "<unknown>"
+    for key in key_path:
+        keys.append(key)
+        pathstr = " -> ".join(keys)
+        try:
+            subconfig = config[key]
+        except KeyError:
+            log.error("Bad config path: %s", pathstr)
+            raise
+        if not isinstance(subconfig, dict):
+            log.error("Value at %s must be a dictionary", pathstr)
+            sys.exit(1)
+        config = subconfig
+    return config
