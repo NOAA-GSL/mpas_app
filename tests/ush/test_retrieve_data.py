8f97fe2b448484b0bc25dac014f0a5c391c6e94d
Christina Holt <christina.holt@noaa.gov>
2025-05-04 15:56:58 -0600

    Test passing.

--- tests/ush/test_retrieve_data.py
+++ tests/ush/test_retrieve_data.py
@@ -1,3 +1,10 @@
+import datetime as dt
+import sys
+from io import StringIO
+from pathlib import Path
+
+from uwtools.api.template import render
+
 from ush import retrieve_data
 
 
@@ -5,10 +12,33 @@ def test_import():
     assert retrieve_data
 
 
-def test_try_data_store():
-    success = try_data_store(
+def test_try_data_store_disk(tmp_path):
+    file_path = tmp_path / "{{ cycle.strftime('%Y%m%d%H') }}"
+    output_path = tmp_path / "output"
+    output_path.mkdir()
+    tmp_file = "tmp.f{{ lead_time.seconds / 3600 }}"
+    existing_file_template = file_path / tmp_file
+    cycle = dt.datetime.now(tz=dt.timezone.utc)
+    lead_times = [dt.timedelta(hours=6), dt.timedelta(hours=12)]
+
+    for lead_time in lead_times:
+        input_stream = StringIO(str(existing_file_template))
+        sys.stdin = input_stream
+        values = {
+            "cycle": cycle,
+            "lead_time": lead_time,
+        }
+        new_file = Path(render(values_src=values, stdin_ok=True))
+        new_file.parent.mkdir(parents=True, exist_ok=True)
+        new_file.touch()
+
+    success = retrieve_data.try_data_store(
+        cycle=cycle,
         data_store="disk",
-        cycle=dt.now(),
-        lead_times=[dt.timedelta(hours=6), dt.timedelta(hours=12)],
+        file_path=file_path,
+        file_templates=[tmp_file],
+        lead_times=lead_times,
+        members=[0],
+        output_path=output_path,
     )
-    assert success is True
+    assert (output_path / new_file.name).is_file()
--- ush/retrieve_data.py
+++ ush/retrieve_data.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# ruff: noqa: ARG001, PLR0913, F841
+
 """
 This script helps users pull data from known data streams, including
 URLS and HPSS (only on supported NOAA platforms), or from user-supplied
@@ -25,23 +27,32 @@ To see usage for this script:
 Also see the parse_args function below.
 """
 
+from __future__ import annotations
+
 import argparse
 import datetime as dt
 import glob
 import logging
 import os
+import re
 import shutil
 import subprocess
 import sys
-import time
-import urllib.request
-from copy import deepcopy
 from pathlib import Path
-from textwrap import dedent
+from typing import NoReturn
 
-import yaml
+from uwtools.api import fs
+from uwtools.api.config import YAMLConfig, get_yaml_config
 
-from uwtools.api.config import YAMLConfig
+
+def _abort(msg: str) -> NoReturn:
+    """
+    Exit with an informative message and error status.
+
+    :param msg: The message to print.
+    """
+    print(msg, file=sys.stderr)
+    sys.exit(1)
 
 
 def clean_up_output_dir(expected_subdir, local_archive, output_path, source_paths):
@@ -79,76 +90,7 @@ def clean_up_output_dir(expected_subdir, local_archive, output_path, source_path
     return unavailable
 
 
-def copy_file(source, destination, copy_cmd):
-    """
-    Copy a file from a source and place it in the destination location.
-    Return a boolean value reflecting the state of the copy.
-
-    Assumes destination exists.
-    """
-
-    if not Path(source).exists():
-        msg = "File does not exist on disk \n %s \n try using: --input_file_path <your_path>"
-        logging.info(msg, source)
-        return False
-
-    # Using subprocess here because system copy is much faster than
-    # python copy options.
-    cmd = f"{copy_cmd} {source} {destination}"
-    logging.info("Running command: \n %s", cmd)
-    try:
-        subprocess.run(
-            cmd,
-            check=True,
-            shell=True,
-        )
-    except subprocess.CalledProcessError as err:
-        logging.info(err)
-        return False
-    return True
-
-
-def check_file(url):
-    """
-    Check that a file exists at the expected URL. Return boolean value
-    based on the response.
-    """
-    status_code = urllib.request.urlopen(url).getcode()
-    return status_code == 200
-
-
-def download_file(url):
-    """
-    Download a file from a url source, and place it in a target location
-    on disk.
-
-    Arguments:
-      url          url to file to be downloaded
-
-    Return:
-      boolean value reflecting state of download.
-
-    """
-
-    # wget flags:
-    # -c continue previous attempt
-    # -T timeout seconds
-    # -t number of tries
-    cmd = f"wget -q -c -T 15 -t 2 {url}"
-    logging.debug("Running command: \n %s", cmd)
-    try:
-        subprocess.run(cmd, check=True, shell=True)
-    except subprocess.CalledProcessError as err:
-        logging.info(err)
-        return False
-    except:
-        logging.error("Command failed!")
-        raise
-
-    return True
-
-
-def _arg_list_to_range(args):
+def _arg_list_to_range(args: list[int | str] | str) -> list:
     """
     Given an argparse list argument, return the sequence to process.
 
@@ -164,13 +106,14 @@ def _arg_list_to_range(args):
     Must ensure that the list contains integers.
     """
 
-    args = args if isinstance(args, list) else list(args)
-    arg_len = len(args)
+    arg_list = args.split(" ") if isinstance(args, str) else args
+    arg_vals = [int(i) for i in arg_list]
+    arg_len = len(arg_vals)
     if arg_len in (2, 3):
-        args[1] += 1
-        return list(range(*args))
+        arg_vals[1] += 1
+        return list(range(*arg_vals))
 
-    return args
+    return arg_vals
 
 
 def _timedelta_from_str(tds: str) -> dt.timedelta:
@@ -182,7 +125,7 @@ def _timedelta_from_str(tds: str) -> dt.timedelta:
     if matches := re.match(r"(\d+)(:(\d+))?(:(\d+))?", tds):
         h, m, s = [int(matches.groups()[n] or 0) for n in (0, 2, 4)]
         return dt.timedelta(hours=h, minutes=m, seconds=s)
-    _abort(f"Specify leadtime as hours[:minutes[:seconds]]")
+    _abort("Specify leadtime as hours[:minutes[:seconds]]")
 
 
 def expand_template(template, config, cycles, lead_times, members):
@@ -193,7 +136,7 @@ def expand_template(template, config, cycles, lead_times, members):
     for cycle in cycles:
         for lead_time in lead_times:
             for member in members:
-                cfg = get_yaml_config({"file": tmpl})
+                cfg = get_yaml_config({"file": template})
                 cfg.dereference(
                     context={
                         "cycle": cycle,
@@ -223,29 +166,6 @@ def write_summary_file(cla, data_store, file_templates):
     the data was retrieved, write a bash summary file that is needed by
     the workflow elements downstream."""
 
-    members = cla.members if isinstance(cla.members, list) else [-1]
-    for mem in members:
-        files = []
-        for tmpl in file_templates:
-            for t in tmpl if isinstance(tmpl, list) else [tmpl]:
-                files.extend(
-                    [fill_template(t, cla.cycle_date, fcst_hr=fh, mem=mem) for fh in cla.fcst_hrs]
-                )
-        output_path = fill_template(cla.output_path, cla.cycle_date, mem=mem)
-        summary_fp = Path(output_path, cla.summary_file)
-        logging.info("Writing a summary file to %s", summary_fp)
-        file_contents = dedent(
-            f"""
-            DATA_SRC={data_store}
-            EXTRN_MDL_CDATE={cla.cycle_date.strftime("%Y%m%d%H")}
-            EXTRN_MDL_STAGING_DIR={output_path}
-            EXTRN_MDL_FNS=( {" ".join(files)} )
-            EXTRN_MDL_FHRS=( {" ".join([str(i) for i in cla.fcst_hrs])} )
-            """
-        )
-        logging.info("Contents: %s", file_contents)
-        summary_fp.write_text(file_contents)
-
 
 def get_ens_groups(members):
     """Given a list of ensemble members, return a dict with keys for
@@ -272,15 +192,7 @@ def parse_args(argv):
     argument.
     """
 
-    description = (
-        "Allowable Python templates for paths, urls, and file names are "
-        " defined in the fill_template function and include:\n"
-        f"{'-' * 120}\n"
-        f"{fill_template('null', dt.datetime.now(tz=dt.timezone.utc), templates_only=True)}"
-    )
-    parser = argparse.ArgumentParser(
-        description=description,
-    )
+    parser = argparse.ArgumentParser()
 
     # Required
     parser.add_argument(
@@ -300,9 +212,9 @@ def parse_args(argv):
     )
     parser.add_argument(
         "--cycle",
-        help="The cycle in ISO8601 format (e.g. %s)" % cycle.strftime("%Y-%m-%dT%H"),
+        help="The cycle in ISO8601 format (e.g. yyyy-mm-ddThh)",
         required=False,
-        default=dt.datetime.now(),
+        default=dt.datetime.now(tz=dt.timezone.utc),
         type=dt.datetime.fromisoformat,
     )
     parser.add_argument(
@@ -311,7 +223,6 @@ def parse_args(argv):
         help="List of priority data_stores. Tries first list item first.",
         nargs="*",
         required=True,
-        type=to_lower,
     )
     parser.add_argument(
         "--data_type",
@@ -392,13 +303,6 @@ def parse_args(argv):
         help="Name of the summary file to be written to the output \
         directory",
     )
-    parser.add_argument(
-        "--check_file",
-        action="store_true",
-        help="Use this flag to check the existence of requested files, \
-         but don't try to download them. Works with download protocol \
-         only",
-    )
 
     # Make modifications/checks for given values
     args = parser.parse_args(argv)
@@ -409,18 +313,15 @@ def parse_args(argv):
         raise argparse.ArgumentTypeError(msg)
 
     # If disk was in data_stores, make sure a path was provided
-    if "disk" in args.data_stores:
-        # Make sure a path was provided.
-        if not args.input_file_path:
-            raise argparse.ArgumentTypeError(
-                ("You must provide an input_file_path when choosing  disk as a data store!")
-            )
+    if "disk" in args.data_storesi and not args.input_file_path:
+        msg = "You must provide an input_file_path when choosing  disk as a data store!"
+        raise argparse.ArgumentTypeError(msg)
 
     # Ensure hpss module is loaded if hpss is included in data stores
-    if "hpss" in cla.data_stores:
+    if "hpss" in args.data_stores:
         try:
             subprocess.run(
-                "which hsi",
+                "/usr/bin/which hsi",
                 check=True,
                 shell=True,
             )
@@ -435,19 +336,19 @@ def parse_args(argv):
 
 
 def retrieve_data(
-    cycle: datetime,
+    config: YAMLConfig | None,
+    cycle: dt.datetime,
     data_stores: list,
     data_type: str,
     file_set: str,
-    config: Optional[YAMLConfig],
-    check_file: bool = False,
-    file_fmt: Optional[str] = None,
-    fcst_hrs: Optional[list] = None,
-    file_templates: Optional[list] = None,
-    ics_or_lbcs: Optional[str] = None,
-    input_file_path: Optional[str] = None,
-    members: Optional[list[int]] = None,
-    summary_file: Optional[str, Path] = None,
+    output_path: Path,
+    file_templates: list[str | None],
+    lead_times: list[int | None],
+    members: list[int | None],
+    file_fmt: str | None = None,
+    ics_or_lbcs: str | None = None,
+    input_file_path: str | None = None,
+    summary_file: str | Path | None = None,
 ) -> bool:
     """
     Checks for and gathers the requested data.
@@ -456,18 +357,20 @@ def retrieve_data(
     for store in data_stores:
         args = {
             "data_store": store,
-            "cycle": cyle,
-            "lead_times": fcst_hrs,
+            "cycle": cycle,
+            "lead_times": lead_times,
             "members": members,
+            "output_path": output_path,
         }
         if store == "disk":
             args.update(
                 {
                     "file_path": input_file_path,
-                    "file_templates": input_file_templates,
+                    "file_templates": file_templates,
                 }
             )
         else:
+            assert config is not None
             args.update(
                 {
                     "config": config[data_type][store],
@@ -476,19 +379,21 @@ def retrieve_data(
                 }
             )
         success = try_data_store(**args)
+    return success
 
 
 def try_data_store(
-    data_store: str,
     cycle: dt.datetime,
+    data_store: str,
+    file_templates: list[str | None],
     lead_times: list[dt.timedelta],
-    members: Optional[list[str]] = None,
-    file_path: Optional[Path] = None,
-    file_templates: Optional[list[str]] = None,
-    config: Optional[YAMLConfig] = None,
-    file_set: Optional[str] = None,
-    file_format: Optional[str] = None,
-):
+    members: list[int | None],
+    output_path: Path,
+    config: YAMLConfig | None = None,
+    file_format: str | None = None,
+    file_path: Path | None = None,
+    file_set: str | None = None,
+) -> bool:
     """
     Given a data store, prepare a UW YAML file block to retrieve all requested
     data. Iterate through each potential option until the data set is retrieved.
@@ -499,6 +404,23 @@ def try_data_store(
         assert file_templates is not None
 
     # form a UW YAML to try a copy
+    fs_config: dict[str, str] = {}
+    for member in members:
+        for lead_time in lead_times:
+            for file_template in file_templates:
+                file_item = get_yaml_config({file_template: f"{file_path}/{file_template}"})
+                file_item.dereference(
+                    context={
+                        "cycle": cycle,
+                        "lead_time": lead_time,
+                        "member": member,
+                    }
+                )
+                fs_config.update(file_item)
+    files_copied = fs.copy(
+        config=fs_config, target_dir=output_path, cycle=cycle, leadtime=lead_time
+    )
+    logging.info(files_copied)
 
     return False
 
@@ -506,9 +428,9 @@ def try_data_store(
 def main(args):
     clargs = parse_args(args)
 
-    setup_logging(debug)
+    setup_logging(clargs.debug)
     print("Running script retrieve_data.py with args:", f"\n{('-' * 80)}\n{('-' * 80)}")
-    for name, val in cla.__dict__.items():
+    for name, val in clargs.__dict__.items():
         if name not in ["config"]:
             print(f"{name:>15s}: {val}")
     print(f"{('-' * 80)}\n{('-' * 80)}")
@@ -519,13 +441,13 @@ def main(args):
         data_stores=clargs.data_stores,
         data_type=clargs.data_type,
         file_set=clargs.file_set,
-        check_file=clargs.check_file,
         file_fmt=clargs.file_fmt,
         lead_times=clargs.lead_times,
         file_templates=clargs.file_templates,
         ics_or_lbcs=clargs.ics_or_lbcs,
         input_file_path=clargs.input_file_path,
         members=clargs.members,
+        output_path=clargs.output_path,
         summary_file=clargs.summary_file,
     )
 
