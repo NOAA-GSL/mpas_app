7c613ced5cad75122f3a4258614f971bb85f961f
Christina Holt <christina.holt@noaa.gov>
2025-05-05 12:37:18 -0600

    WIP. Not rendering for prepare_fs_copy.

--- tests/ush/test_retrieve_data.py
+++ tests/ush/test_retrieve_data.py
@@ -2,17 +2,25 @@ import datetime as dt
 import sys
 from io import StringIO
 from pathlib import Path
+from unittest.mock import Mock, patch
 
+from pytest import fixture
+from uwtools.api.config import get_yaml_config
 from uwtools.api.template import render
 
 from ush import retrieve_data
+
+
+@fixture
+def data_locations():
+    return get_yaml_config(Path(f"{__file__}/../../../parm/data_locations.yml").resolve())
 
 
 def test_import():
     assert retrieve_data
 
 
-def test_try_data_store_disk(tmp_path):
+def test_try_data_store_disk_success(tmp_path):
     file_path = tmp_path / "{{ cycle.strftime('%Y%m%d%H') }}"
     output_path = tmp_path / "output"
     output_path.mkdir()
@@ -21,6 +29,7 @@ def test_try_data_store_disk(tmp_path):
     cycle = dt.datetime.now(tz=dt.timezone.utc)
     lead_times = [dt.timedelta(hours=6), dt.timedelta(hours=12)]
 
+    new_files = []
     for lead_time in lead_times:
         input_stream = StringIO(str(existing_file_template))
         sys.stdin = input_stream
@@ -31,14 +40,55 @@ def test_try_data_store_disk(tmp_path):
         new_file = Path(render(values_src=values, stdin_ok=True))
         new_file.parent.mkdir(parents=True, exist_ok=True)
         new_file.touch()
+        new_files.append(new_file)
 
-    success = retrieve_data.try_data_store(
+    success, _ = retrieve_data.try_data_store(
+        config=get_yaml_config({}),
         cycle=cycle,
-        data_store="disk",
-        file_path=file_path,
         file_templates=[tmp_file],
         lead_times=lead_times,
+        locations=[file_path],
         members=[0],
-        output_path=output_path,
+        outpath=output_path,
     )
-    assert (output_path / new_file.name).is_file()
+    assert all([(output_path / f.name).is_file() for f in new_files])
+    assert success
+
+
+def test_try_data_store_disk_fail(tmp_path):
+    file_path = tmp_path / "{{ cycle.strftime('%Y%m%d%H') }}"
+    output_path = tmp_path / "output"
+    output_path.mkdir()
+    tmp_file = "tmp.f{{ lead_time.seconds / 3600 }}"
+    existing_file_template = file_path / tmp_file
+    cycle = dt.datetime.now(tz=dt.timezone.utc)
+    lead_times = [dt.timedelta(hours=6), dt.timedelta(hours=12)]
+
+    success, _ = retrieve_data.try_data_store(
+        config=get_yaml_config({}),
+        cycle=cycle,
+        file_templates=[tmp_file],
+        lead_times=lead_times,
+        locations=[file_path],
+        members=[0],
+        outpath=output_path,
+    )
+    assert not success
+
+
+def test_prepare_fs_copy_config_gfs_grib2_aws(data_locations, tmp_path):
+    output_path = tmp_path / "output"
+    output_path.mkdir()
+
+    cycle = dt.datetime.now(tz=dt.timezone.utc)
+    lead_times = [dt.timedelta(hours=6), dt.timedelta(hours=12)]
+    config = retrieve_data.prepare_fs_copy_config(
+        config=data_locations,
+        cycle=cycle,
+        file_templates=data_locations["GFS"]["file_names"]["fcst"]["netcdf"],
+        lead_times=lead_times,
+        location=data_locations["GFS"]["aws"]["locations"][0],
+        members=[0],
+    )
+    print(config)
+    assert not config
--- ush/retrieve_data.py
+++ ush/retrieve_data.py
@@ -31,18 +31,20 @@ from __future__ import annotations
 
 import argparse
 import datetime as dt
-import glob
 import logging
 import os
 import re
-import shutil
 import subprocess
 import sys
-from pathlib import Path
-from typing import NoReturn
+from typing import TYPE_CHECKING, NoReturn
 
 from uwtools.api import fs
 from uwtools.api.config import YAMLConfig, get_yaml_config
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+FILE_SETS = ("anl", "fcst", "obs", "fix")
 
 
 def _abort(msg: str) -> NoReturn:
@@ -53,41 +55,6 @@ def _abort(msg: str) -> NoReturn:
     """
     print(msg, file=sys.stderr)
     sys.exit(1)
-
-
-def clean_up_output_dir(expected_subdir, local_archive, output_path, source_paths):
-    """Remove expected sub-directories and existing_archive files on
-    disk once all files have been extracted and put into the specified
-    output location."""
-
-    unavailable = {}
-    expand_source_paths = []
-    logging.debug("Cleaning up local paths: %s", source_paths)
-    for p in source_paths:
-        expand_source_paths.extend(glob.glob(p.lstrip("/")))
-
-    # Check to make sure the files exist on disk
-    for file_path in expand_source_paths:
-        local_file_path = str(Path.cwd() / file_path.lstrip("/"))
-        logging.debug("Moving %s to %s", local_file_path, output_path)
-        if not Path(local_file_path).exists():
-            logging.info("File does not exist: %s", local_file_path)
-            unavailable["hpss"] = expand_source_paths
-        else:
-            file_name = Path(file_path).name
-            expected_output_loc = str(Path(output_path, file_name))
-            if local_file_path != expected_output_loc:
-                logging.info("Moving %s to %s", local_file_path, expected_output_loc)
-                shutil.move(local_file_path, expected_output_loc)
-
-    # Clean up directories from inside archive, if they exist
-    if Path(expected_subdir).exists() and expected_subdir != "./":
-        logging.info("Removing %s", expected_subdir)
-        os.removedirs(expected_subdir)
-
-    # If an archive exists on disk, remove it
-    Path(local_archive).unlink(missing_ok=True)
-    return unavailable
 
 
 def _arg_list_to_range(args: list[int | str] | str) -> list:
@@ -197,7 +164,7 @@ def parse_args(argv):
     # Required
     parser.add_argument(
         "--file_set",
-        choices=("anl", "fcst", "obs", "fix"),
+        choices=FILE_SETS,
         help="Flag for whether analysis, forecast, \
         fix, or observation files should be gathered",
         required=True,
@@ -336,93 +303,109 @@ def parse_args(argv):
 
 
 def retrieve_data(
-    config: YAMLConfig | None,
+    config: YAMLConfig,
     cycle: dt.datetime,
-    data_stores: list,
+    data_stores: list[str],
     data_type: str,
     file_set: str,
-    output_path: Path,
+    outpath: Path,
     file_templates: list[str | None],
-    lead_times: list[int | None],
+    lead_times: list[dt.timedelta],
     members: list[int | None],
     file_fmt: str | None = None,
     ics_or_lbcs: str | None = None,
-    input_file_path: str | None = None,
+    inpath: Path | None = None,
     summary_file: str | Path | None = None,
 ) -> bool:
     """
     Checks for and gathers the requested data.
     """
 
+    standard_file_names = config[data_type]["file_names"][file_set]
+    standard_file_names = (
+        standard_file_names.get(file_fmt)
+        if isinstance(standard_file_names, dict)
+        else standard_file_names
+    )
     for store in data_stores:
-        args = {
-            "data_store": store,
-            "cycle": cycle,
-            "lead_times": lead_times,
-            "members": members,
-            "output_path": output_path,
-        }
+        # checks for given data_store
         if store == "disk":
-            args.update(
-                {
-                    "file_path": input_file_path,
-                    "file_templates": file_templates,
-                }
-            )
+            assert inpath is not None
         else:
-            assert config is not None
-            args.update(
-                {
-                    "config": config[data_type][store],
-                    "file_set": file_set,
-                    "file_format": file_fmt,
-                }
-            )
-        success = try_data_store(**args)
+            assert config
+            assert file_set in FILE_SETS
+
+        success, files_copied = try_data_store(
+            config=config,
+            cycle=cycle,
+            file_templates=file_templates or standard_file_names,
+            lead_times=lead_times,
+            locations=[inpath] if inpath else config[data_type][store]["locations"],
+            members=members,
+            outpath=outpath,
+        )
     return success
 
 
-def try_data_store(
+def prepare_fs_copy_config(
+    config: YAMLConfig,
     cycle: dt.datetime,
-    data_store: str,
     file_templates: list[str | None],
     lead_times: list[dt.timedelta],
+    location: Path,
     members: list[int | None],
-    output_path: Path,
-    config: YAMLConfig | None = None,
-    file_format: str | None = None,
-    file_path: Path | None = None,
-    file_set: str | None = None,
-) -> bool:
+) -> dict[str, str]:
+    fs_copy_config: dict[str, str] = {}
+    for member in members:
+        for lead_time in lead_times:
+            for file_template in file_templates:
+                file_item = get_yaml_config({file_template: f"{location}/{file_template}"})
+                file_item.dereference(
+                    context={
+                        "cycle": cycle,
+                        "fcst_hr": lead_time.seconds / 3600,
+                        "lead_time": lead_time,
+                        "member": member,
+                        **config,
+                    }
+                )
+                print(file_item)
+                fs_copy_config.update(file_item)
+    return fs_copy_config
+
+
+def try_data_store(
+    config: YAMLConfig,
+    cycle: dt.datetime,
+    file_templates: list[str | None],
+    lead_times: list[dt.timedelta],
+    locations: list[Path],
+    members: list[int | None],
+    outpath: Path,
+) -> tuple[bool, dict[str, str]]:
     """
     Given a data store, prepare a UW YAML file block to retrieve all requested
     data. Iterate through each potential option until the data set is retrieved.
     """
-    # check for correct data_store
-    if data_store == "disk":
-        assert file_path is not None
-        assert file_templates is not None
 
     # form a UW YAML to try a copy
-    fs_config: dict[str, str] = {}
-    for member in members:
-        for lead_time in lead_times:
-            for file_template in file_templates:
-                file_item = get_yaml_config({file_template: f"{file_path}/{file_template}"})
-                file_item.dereference(
-                    context={
-                        "cycle": cycle,
-                        "lead_time": lead_time,
-                        "member": member,
-                    }
-                )
-                fs_config.update(file_item)
-    files_copied = fs.copy(
-        config=fs_config, target_dir=output_path, cycle=cycle, leadtime=lead_time
-    )
-    logging.info(files_copied)
+    for location in locations:
+        fs_copy_config = prepare_fs_copy_config(
+            config=config,
+            cycle=cycle,
+            file_templates=file_templates,
+            lead_times=lead_times,
+            location=location,
+            members=members,
+        )
+        files_copied = fs.copy(config=fs_copy_config, target_dir=outpath, cycle=cycle)
+        if files_copied["ready"] and not files_copied["not-ready"]:
+            logging.info(files_copied)
+            print(files_copied)
+            print(fs_copy_config)
+            return True, fs_copy_config
 
-    return False
+    return False, fs_copy_config
 
 
 def main(args):
@@ -445,9 +428,9 @@ def main(args):
         lead_times=clargs.lead_times,
         file_templates=clargs.file_templates,
         ics_or_lbcs=clargs.ics_or_lbcs,
-        input_file_path=clargs.input_file_path,
+        inpath=clargs.input_file_path,
         members=clargs.members,
-        output_path=clargs.output_path,
+        outpath=clargs.output_path,
         summary_file=clargs.summary_file,
     )
 
