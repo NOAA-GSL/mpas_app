7cbe7ee5070ec6597daa98debcc460218fff1709
Christina Holt <Christina.Holt@noaa.gov>
2025-05-30 17:04:43 +0000

    Adding new ursa files.

---
+++ modulefiles/build_ursa_intel_ifx.lua
@@ -0,0 +1,19 @@
+help([[
+This module loads libraries for building the MPAS App on
+the NOAA RDHPC machine Ursa using Intel-2023.2.0
+]])
+
+--[[whatis([===[Loads libraries needed for building the MPAS App on Ursa ]===])]]
+
+load("cmake/3.30.2")
+load("intel-oneapi-compilers/2024.2.1")
+load("intel-oneapi-mpi/2021.13.1")
+load("parallel-netcdf/1.12.3")
+
+--[[ This path comes from the module's PARALLEL_NETCDF_ROOT env variable]]
+setenv("PNETCDF", "/apps/spack-2024-12/linux-rocky9-x86_64/oneapi-2024.2.1/parallel-netcdf-1.12.3-xynwqn25sqarwdvdmsoejsryxdyj73x4/")
+
+setenv("CMAKE_C_COMPILER", "mpiicc")
+setenv("CMAKE_CXX_COMPILER", "mpiicpc")
+setenv("CMAKE_Fortran_COMPILER", "mpiifort")
+
