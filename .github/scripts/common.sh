ad58fdca0defc86587bc9929e2b7c59b3dba86f4
elcarpenterNOAA <emily.l.carpenter@noaa.gov>
2025-06-09 15:43:22 -0400

    paul suggestions

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -5,7 +5,7 @@ CI_CONDA_SH=$CI_CONDA_DIR/etc/profile.d/conda.sh
 
 ci_conda_activate() {
   . "$CI_CONDA_SH"
-  if [ -z "$1" ]; then
+  if [[ -z "$1" ]]; then
     conda activate
   else
     conda activate "$1"
