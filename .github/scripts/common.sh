dd72cec3adcc74e0581abf5a133d7456ed0d8c30
elcarpenterNOAA <emily.l.carpenter@noaa.gov>
2025-06-09 15:50:34 -0400

    single tmp

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -1,7 +1,6 @@
 # Shared resources for workflow scripts.
 
-CI_CONDA_DIR=/tmp/conda
-CI_CONDA_SH=$CI_CONDA_DIR/etc/profile.d/conda.sh
+CI_CONDA_SH="$CI_CONDA_DIR/etc/profile.d/conda.sh"
 
 ci_conda_activate() {
   . "$CI_CONDA_SH"
