15db3a581a0658109d4411a75676be85b8ba7782
elcarpenterNOAA <emily.l.carpenter@noaa.gov>
2025-06-09 14:53:23 -0400

    WIP

--- .github/scripts/common.sh
+++ .github/scripts/common.sh
@@ -4,6 +4,10 @@ CI_CONDA_DIR=/tmp/conda
 CI_CONDA_SH=$CI_CONDA_DIR/etc/profile.d/conda.sh
 
 ci_conda_activate() {
-  . $CI_CONDA_SH
-  conda activate
+  . "$CI_CONDA_SH"
+  if [ -z "$1" ]; then
+    conda activate
+  else
+    conda activate "$1"
+  fi
 }
