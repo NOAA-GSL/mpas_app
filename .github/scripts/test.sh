c893b3b99ad7a7391b0a4205eefc6d5446931738
Paul Madden <paul.madden@noaa.gov>
2025-06-26 19:27:48 +0000

    Revamp .github

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,5 +1,6 @@
-set -eux
-. "$(dirname "${BASH_SOURCE[0]}")/common.sh"
-ci_conda_activate mpas_app
+set -eu
+source conda/etc/profile.d/conda.sh
+conda activate mpas_app
+set -x
 git clean -dfx
 make test
