208e0f1405ee987681182786111dbc69295b3985
elcarpenterNOAA <emily.l.carpenter@noaa.gov>
2025-07-15 17:47:20 -0400

    WIP

--- scripts/get_external_data.sh
+++ scripts/get_external_data.sh
@@ -11,15 +11,26 @@ else
   fcst_hours="${first_time} ${last_time} ${LBC_INTVL_HRS}"
 fi
 
+additional_args=()
+if [[ -n "${INPUT_FILE_PATH:-}" ]]; then
+  additional_args+=(--input-file-path "${INPUT_FILE_PATH}")
+fi
+
+file_template_args=""
+if [[ -n "${FILE_TEMPLATES:-}" ]]; then
+  additional_args+=(--file-templates "${FILE_TEMPLATES}")
+fi
+
 set -x
 python -u ${MPAS_APP}/ush/retrieve_data.py \
     --debug \
     --fileset ${fileset:-fcst} \
     --config ${MPAS_APP}/parm/data_locations.yml \
     --cycle ${cycle} \
-    --data-stores aws \
+    --data-stores ${DATA_STORES} \
     --data-type ${EXTERNAL_MODEL} \
     --fcst-hrs $fcst_hours \
     --filefmt grib2 \
-    --output-path ${OUTPUT_PATH}
+    --output-path ${OUTPUT_PATH} \
+    "${additional_args[@]}"
 
