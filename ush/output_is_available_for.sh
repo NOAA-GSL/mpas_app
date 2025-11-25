9cf582e96fe8d73b34fe7cf3647024a5b1e56a44
Samuel.Trahan <Samuel.Trahan@noaa.gov>
2025-11-25 21:51:54 +0000

    simplify output_is_available_for.sh

--- ush/output_is_available_for.sh
+++ ush/output_is_available_for.sh
@@ -3,16 +3,12 @@
 forecast_dir="$1"
 start_time="$2"
 lead_time="$3"
-add_to_lead_time="$4"
+output_interval="$4"
 
-eval "check_time=\$(( 10#\$lead_time + 10#\$add_to_lead_time ))"
-
-# 2025-08-22t00:00:00 UTC+0
-s="$start_time"
-start_posix="${s:0:4}-${s:5:2}-${s:8:2}t${s:11:2}:${s:14:2}:${s:17:2} UTC+0"
+eval "check_time=\$(( 10#\$lead_time + 10#\$output_interval ))"
 
 # 2025-08-23_06.00.00
-valid_time_posix=$( date +"%Y-%m-%d_%H.%M.%S" -d "$start_posix + $check_time hours" )
+valid_time_posix=$( date +"%Y-%m-%d_%H.%M.%S" -d "$start_time UTC+0 + $check_time hours" )
 
 history="$forecast_dir/history.$valid_time_posix.nc"
 diag="$forecast_dir/diag.$valid_time_posix.nc"
