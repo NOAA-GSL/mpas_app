45dd8027e1622cd26bad9df55eb78fcd6d524682
Samuel.Trahan <Samuel.Trahan@noaa.gov>
2025-11-25 22:04:49 +0000

    remove eval and explain an exec

--- ush/output_is_available_for.sh
+++ ush/output_is_available_for.sh
@@ -5,7 +5,7 @@ start_time="$2"
 lead_time="$3"
 output_interval="$4"
 
-eval "check_time=\$(( 10#\$lead_time + 10#\$output_interval ))"
+check_time=$(( "10#$lead_time" + "10#$output_interval" ))
 
 # 2025-08-23_06.00.00
 valid_time_posix=$( date +"%Y-%m-%d_%H.%M.%S" -d "$start_time UTC+0 + $check_time hours" )
@@ -13,4 +13,5 @@ valid_time_posix=$( date +"%Y-%m-%d_%H.%M.%S" -d "$start_time UTC+0 + $check_tim
 history="$forecast_dir/history.$valid_time_posix.nc"
 diag="$forecast_dir/diag.$valid_time_posix.nc"
 
+# Return status of this script should be return status of the test:
 exec test -s "$history" -a -s "$diag"
