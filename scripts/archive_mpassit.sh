6ea2792c2eaa7821398a39242f4ce7a6897c012e
Samuel.Trahan <Samuel.Trahan@noaa.gov>
2025-11-25 21:50:34 +0000

    YMDH => yyyymmddhh

--- scripts/archive_mpassit.sh
+++ scripts/archive_mpassit.sh
@@ -3,23 +3,23 @@
 set -xue
 
 day=$1
-YMDH=$2
+yyyymmddhh=$2
 archive_dir=$3
 
 skip_hours=$(( ( day - 1 ) * 24 ))
 
-YYYY=${YMDH:0:4}
-MM=${YMDH:4:2}
-DD=${YMDH:6:2}
-HH=${YMDH:8:2}
+YYYY=${yyyymmddhh:0:4}
+MM=${yyyymmddhh:4:2}
+DD=${yyyymmddhh:6:2}
+HH=${yyyymmddhh:8:2}
 
 day_prefix=$( date +"MPAS-A_out.%Y-%m-%d" -d "${YYYY}-${MM}-${DD}t00:00:00 UTC+0 + $skip_hours hours" )
-archive_basename=${YMDH}-mpassit-$( date +"%Y%m%d" -d "${YYYY}-${MM}-${DD}t00:00:00 UTC+0 + $skip_hours hours" ).tar
+archive_basename=${yyyymmddhh}-mpassit-$( date +"%Y%m%d" -d "${YYYY}-${MM}-${DD}t00:00:00 UTC+0 + $skip_hours hours" ).tar
 
 archive="$archive_dir/$archive_basename"
 
 set +e
-listing=$( ls -1 $YMDH/mpassit/*/"$day_prefix"* )
+listing=$( ls -1 $yyyymmddhh/mpassit/*/"$day_prefix"* )
 set -e
 
 if [[ -z "${listing:-}" ]] ; then
